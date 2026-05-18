cff4458a485ae93d4d2acfc389cfbd51a2fc684c
kate-gh <kawtargantouh@gmail.com>
2026-05-18 15:35:56 +0100

    improve Kafka event

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,12 +1,30 @@
 from kafka import KafkaProducer
 import json
 
-producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
-)
+producer = None
+
+def get_producer():
+    global producer
+
+    if producer is None:
+        try:
+            producer = KafkaProducer(
+                bootstrap_servers="localhost:9092",
+                value_serializer=lambda v:
+                json.dumps(v).encode("utf-8")
+            )
+
+            print("[KAFKA] connected")
+
+        except Exception as e:
+            print(f"[KAFKA] unavailable: {e}")
+            producer = False
+
+    return producer
+
 
 def publish_kb_update(question, answer, category, priority):
+
     data = {
         "question": question,
         "answer": answer,
@@ -14,7 +32,19 @@ def publish_kb_update(question, answer, category, priority):
         "priority": priority
     }
 
-    producer.send("kb_updates", data)
-    producer.flush()
+    p = get_producer()
 
-    print(" [KAFKA] envoyé :", question)
+    if not p:
+        print("[KAFKA] skipped")
+        return
+
+    try:
+
+        p.send("kb_updates", data)
+        p.flush()
+
+        print("[KAFKA] envoyé :", question)
+
+    except Exception as e:
+
+        print("[KAFKA ERROR]", e)
